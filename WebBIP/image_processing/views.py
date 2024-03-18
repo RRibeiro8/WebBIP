4fb799a1be0c3a479d84c9752d28580e8d8e576c
RRibeiro8 <rfribeiro@ua.pt>
2024-03-18 22:23:41 +0000

    Add clickable and real time upload

--- WebBIP/image_processing/views.py
+++ WebBIP/image_processing/views.py
@@ -20,14 +20,36 @@ class ImageProcessingView(View):
         print("POST", request)
 
         file = request.FILES.get('image')
-        print("Request: ", file) 
+        if file:
 
-        name = file.name.split('.')[0]
+            name = file.name.split('.')[0]
 
-        print(name)
+            image, created = ImageModel.objects.get_or_create(file=file, name=name)
 
-        image, created = ImageModel.objects.get_or_create(file=file, name=name)
+            context = { "success": True, 
+                        "name": image.name,
+                        "pk": image.pk}
+            return JsonResponse(context, status=200)
+
+        else:
+
+            method = request.POST.getlist('method')[0]
+
+            if method == "selected":
+
+                pk = request.POST.getlist('pk')[0]
+
+                image = ImageModel.objects.filter(pk=pk).first()
+
+                if image:
+
+                    context = { "success": True, 
+                            "img_path": image.file.url}
+
+                    return JsonResponse(context, safe=False, status=200)
 
 
-        return JsonResponse({"success": True}, status=200)
+
+        
+        return JsonResponse({"success": False}, status=400)
 
