029bf9342c8194c681335ce71083674b2250a343
RRibeiro8 <rfribeiro@ua.pt>
2024-03-18 02:41:22 +0000

    Layout and Image Model

--- WebBIP/image_processing/models.py
+++ WebBIP/image_processing/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+def get_upload_path(instance, filename):
+    return "{0}".format(filename)
+
+
+class ImageModel(models.Model):
+
+    file = models.ImageField(upload_to=get_upload_path) 
+    name = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+
+        if not self.name:
+            self.name =  self.file.name.split('.')[0]
+
+        super(ImageModel, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+
+        self.file.delete()
+        super(ImageModel, self).delete(*args, **kwargs)
--- WebBIP/image_processing/views.py
+++ WebBIP/image_processing/views.py
@@ -2,21 +2,32 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 
+from .models import ImageModel
+
 
 class ImageProcessingView(View):
 
     template_name = 'image_processing/index.html'
 
     def get(self, request):
-    
 
-        context = {}
+        context = {"images": ImageModel.objects.all()}
 
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
 
-        print("POST")
+        print("POST", request)
+
+        file = request.FILES.get('image')
+        print("Request: ", file) 
+
+        name = file.name.split('.')[0]
+
+        print(name)
+
+        image, created = ImageModel.objects.get_or_create(file=file, name=name)
+
 
         return JsonResponse({"success": True}, status=200)
 
